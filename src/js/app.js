11970ecb061c274174803da6366ed2bbf6f422d9
Thomas Hockaday <thomas.hockaday@opia-sp.com>
2018-01-09 17:24:02 +0000

    Refactor for coding standards

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,22 @@
 require('../scss/app.scss');
 
-var verticalCarousels = document.querySelectorAll(".vertical-carousel");
+let verticalCarousels = document.querySelectorAll(".vertical-carousel");
 
-verticalCarousels.forEach(function(item){
-	var itemHeight = item.offsetHeight;
-	var topVal = 0;
-	item.style.lineHeight = itemHeight + "px";
-	var items = item.querySelectorAll("li");
-	var interval = window.setInterval(function(){
-		topVal -= itemHeight;
-		if(topVal <= -(items.length * itemHeight)){
-			topVal = 0;
-		}
-		items.forEach(function(li){
-			li.style.top = topVal + "px";
-		});
-	}, 1600);
+verticalCarousels.forEach(function (item) {
+    let itemHeight = item.offsetHeight;
+    let topVal = 0;
+    item.style.lineHeight = itemHeight + "px";
+    let items = item.querySelectorAll("li");
+
+    let interval = window.setInterval(function () {
+        topVal -= itemHeight;
+
+        if (topVal <= -(items.length * itemHeight)) {
+            topVal = 0;
+        }
+
+        items.forEach(function (li) {
+            li.style.top = topVal + "px";
+        });
+    }, 1600);
 });
