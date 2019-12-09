f59231c8a1e62c2d04aa7459bcfbe88b23ce8f42
Thomas Hockaday <thomashockaday95@gmail.com>
2019-12-09 16:21:15 +0000

    Minor cleanup

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-let verticalCarousels = document.querySelectorAll(".vertical-carousel");
+const verticalCarousels = document.querySelectorAll(".vertical-carousel");
 
-verticalCarousels.forEach(function (item) {
-    let itemHeight = item.offsetHeight;
+verticalCarousels.forEach((item) => {
+    const itemHeight = item.offsetHeight;
+    const items = item.querySelectorAll("li");
     let topVal = 0;
-    let items = item.querySelectorAll("li");
 
     item.style.lineHeight = `${itemHeight}px`;
 
-    let interval = window.setInterval(function () {
+    let interval = window.setInterval(() => {
         topVal -= itemHeight;
 
         if (topVal <= -(items.length * itemHeight)) {
             topVal = 0;
         }
 
-        items.forEach(function (li) {
+        items.forEach((li) => {
             li.style.top = `${topVal}px`;
         });
     }, 1600);
